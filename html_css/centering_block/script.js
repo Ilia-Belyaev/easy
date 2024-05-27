a3bab9936efbdc8a1130ef1e505240bf40a02606
Илья Беляев <ilya.belyaev.95@list.ru>
2024-05-27 14:32:36 +0300

    Сохранил варианты gif для отображения погоды, сделал выбор города через ввод. Немного добавил стиля на странице. Начал работу над отобраджением карточки

--- html_css/centering_block/script.js
+++ html_css/centering_block/script.js
@@ -1,24 +1,26 @@
 var but = document.querySelector("button");
 var divText = document.querySelector("div");
-var selected = document.getElementById("Towns");
-        
-        
+//var selected = document.getElementById("Towns");
+var currentTown = document.getElementById("writingTown")       
+ 
 but.addEventListener("click",()=>{
-    divText.textContent = "";
-    var selectedItem = selected.options[selected.selectedIndex].value;   
+    //divText.textContent = "";
+    //var selectedItem = selected.options[selected.selectedIndex].value; 
+    var currentTownValue = currentTown.value?currentTown.value.toLowerCase().replace(currentTown.value.toLowerCase()[0],currentTown.value.toLowerCase()[0].toUpperCase()):"";
+    var body = document.querySelector("body");
     const request = new XMLHttpRequest;
     const requestApi = new XMLHttpRequest();
     const requestWeather = new XMLHttpRequest();
     var apiKey;
     var apiKeyWeather;
     function getApiKey(apiKey){
-        var url = 'https://geocode-maps.yandex.ru/1.x/?apikey='+apiKey+'&geocode='+selectedItem+'&lang=ru-RU'+'&format=json';
+        var url = 'https://geocode-maps.yandex.ru/1.x/?apikey='+apiKey+'&geocode='+currentTownValue+'&lang=ru-RU'+'&format=json';
         request.open("GET",url);
         request.send();
         request.onload=function(){
         if(request.status!=200){
             alert('Ошибка '+ request.status +' : '+ request.statusText);
-            divText.textContent = "Sorry";
+            //divText.textContent = "Sorry";
         }else{
             var coord = JSON.parse(request.response).response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos;
             
@@ -33,10 +35,16 @@ but.addEventListener("click",()=>{
                     if(requestWeather.status!=200){
                         alert('Ошибка '+ requestWeather.status +' : '+ requestWeather.statusText);
                     }else{
-                        console.log(JSON.parse(requestWeather.responseText).main)
-                        divText.innerHTML+= "Weather: "+JSON.parse(requestWeather.responseText).weather[0].description;
-                        divText.innerHTML+= '<br>';
-                        divText.innerHTML+= "Temperature: "+ Math.round(Number(JSON.parse(requestWeather.responseText).main.temp)-273) + '&deg;C' ;
+                        //созхдать новый див и сделать его карточкой с картинкой погоды и результатами
+                        
+                        //var weather = "Weather: "+JSON.parse(requestWeather.responseText).weather[0].description+" Temperature: "+ Math.round(Number(JSON.parse(requestWeather.responseText).main.temp)-273) + '&deg;C';
+                        var weather = JSON.parse(requestWeather.responseText).weather[0].description;
+                        var temp =  Math.round(Number(JSON.parse(requestWeather.responseText).main.temp)-273);
+                        createCard(weather,temp)
+                        
+                        //divText.innerHTML+= "Weather: "+JSON.parse(requestWeather.responseText).weather[0].description;
+                        //divText.innerHTML+= '<br>';
+                        //divText.innerHTML+= "Temperature: "+ Math.round(Number(JSON.parse(requestWeather.responseText).main.temp)-273) + '&deg;C' ;
                     }
                 
             }
@@ -53,6 +61,38 @@ but.addEventListener("click",()=>{
     };
     requestApi.open("GET","ya_api_geocoder.txt",true);
     requestApi.send();
+
+    function createCard(weather,temp){
+        var newCard = document.createElement("div");
+        var weatherIcon = document.createElement("img");
+        weatherIcon.src = chooseWeatherIcon(weather);
+        newCard.innerHTML = weather + temp ;
+        newCard.appendChild(weatherIcon);
+        body.appendChild(newCard);
+    }
+
+    function chooseWeatherIcon(weather){
+        var src = "";
+        var weatherSplit = weather.split(" ");
+        switch (weather[1]){
+            case "wind": src = "weather_icons/wind.gif";
+                break;
+            case "sun": src = "weather_icons/sun.gif";
+                break;
+            case "snow": src = "weather_icons/snow.gif";
+                break;
+            case "rain": src = "weather_icons/rain.gif";
+                break;
+            case "cloud": src = "weather_icons/cloudy.gif";
+                break;
+            default:
+                src = "weather_icons/sun.gif";
+                break;
+        }
+        return src;
+    }
+
+
 
     /*requestWeather.onreadystatechange = function(){
         if(this.status==200){
