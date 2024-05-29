788d0e1299652b0e45ec5497b576cbf94209523e
Илья Беляев <ilya.belyaev.95@list.ru>
2024-05-29 11:01:27 +0300

    Удалил папку с картинками и теперь просто подучаю значок погоды с апи сайта. Немного переделал дизайн. Дизайн в стадии доработки. Еще нужно обновлять содержимое div, вместо добавления нового

--- html_css/centering_block/script.js
+++ html_css/centering_block/script.js
@@ -1,11 +1,8 @@
 var but = document.querySelector("button");
 var divText = document.querySelector("div");
-//var selected = document.getElementById("Towns");
 var currentTown = document.getElementById("writingTown")       
  
 but.addEventListener("click",()=>{
-    //divText.textContent = "";
-    //var selectedItem = selected.options[selected.selectedIndex].value; 
     var currentTownValue = currentTown.value?currentTown.value.toLowerCase().replace(currentTown.value.toLowerCase()[0],currentTown.value.toLowerCase()[0].toUpperCase()):"";
     var body = document.querySelector("body");
     const request = new XMLHttpRequest;
@@ -13,94 +10,63 @@ but.addEventListener("click",()=>{
     const requestWeather = new XMLHttpRequest();
     var apiKey;
     var apiKeyWeather;
-    function getApiKey(apiKey){
-        var url = 'https://geocode-maps.yandex.ru/1.x/?apikey='+apiKey+'&geocode='+currentTownValue+'&lang=ru-RU'+'&format=json';
-        request.open("GET",url);
-        request.send();
-        request.onload=function(){
-        if(request.status!=200){
-            alert('Ошибка '+ request.status +' : '+ request.statusText);
-            //divText.textContent = "Sorry";
-        }else{
-            var coord = JSON.parse(request.response).response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos;
-            
-                var latitude = coord.split(" ")[1];
-                var longitude = coord.split(" ")[0];
-                var url_weather = 'https://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&appid=71dffb53459a694cab374641a0c54649';
-                //var url_weather = "https://api.weather.yandex.ru/v2/forecast?lat="+latitude+"&lon="+longitude+"&lang=ru-RU";
-                requestWeather.open("GET",url_weather);
-                //requestWeather.setRequestHeader('X-Yandex-API-Key', 'd7559011-e187-4a22-9d1c-61febf84397b');
-                requestWeather.send()
-                requestWeather.onload=function(){
-                    if(requestWeather.status!=200){
-                        alert('Ошибка '+ requestWeather.status +' : '+ requestWeather.statusText);
-                    }else{
-                        //созхдать новый див и сделать его карточкой с картинкой погоды и результатами
-                        
-                        //var weather = "Weather: "+JSON.parse(requestWeather.responseText).weather[0].description+" Temperature: "+ Math.round(Number(JSON.parse(requestWeather.responseText).main.temp)-273) + '&deg;C';
-                        var weather = JSON.parse(requestWeather.responseText).weather[0].description;
-                        var temp =  Math.round(Number(JSON.parse(requestWeather.responseText).main.temp)-273);
-                        createCard(weather,temp)
-                        
-                        //divText.innerHTML+= "Weather: "+JSON.parse(requestWeather.responseText).weather[0].description;
-                        //divText.innerHTML+= '<br>';
-                        //divText.innerHTML+= "Temperature: "+ Math.round(Number(JSON.parse(requestWeather.responseText).main.temp)-273) + '&deg;C' ;
+    if (currentTownValue!==""){
+        function getApiKey(apiKey){
+            var url = 'https://geocode-maps.yandex.ru/1.x/?apikey='+apiKey+'&geocode='+currentTownValue+'&lang=ru-RU'+'&format=json';
+            request.open("GET",url);
+            request.send();
+            request.onload=function(){
+                if(request.status!=200){
+                    alert('Ошибка '+ request.status +' : '+ request.statusText);
+                }else{
+                    var coord = JSON.parse(request.response).response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos;
+                    var latitude = coord.split(" ")[1];
+                    var longitude = coord.split(" ")[0];
+                    var url_weather = 'https://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&appid=71dffb53459a694cab374641a0c54649';
+                    requestWeather.open("GET",url_weather);
+                    requestWeather.send()
+                    requestWeather.onload=function(){
+                        if(requestWeather.status!=200){
+                            alert('Ошибка '+ requestWeather.status +' : '+ requestWeather.statusText);
+                        }else{
+                            var weatherImg = JSON.parse(requestWeather.responseText).weather[0].icon;
+                            var weather = JSON.parse(requestWeather.responseText).weather[0].description+", ";
+                            var temp =  Math.round(Number(JSON.parse(requestWeather.responseText).main.temp)-273)+'&deg;C';
+                            createCard(weather,temp,weatherImg)
+                        }
                     }
-                
-            }
-            
+                }
             }
         }
-    }
-    requestApi.onreadystatechange = function(){
-        if(this.status == 200){
-            apiKey = requestApi.responseText;
+        requestApi.onreadystatechange = function(){
+            if(this.status == 200){
+                apiKey = requestApi.responseText;
+                getApiKey(apiKey)
+            }
+        };
+        requestApi.open("GET","ya_api_geocoder.txt",true);
+        requestApi.send();
+    
+        function createCard(weather,temp,weatherImg){
+            var newCard = document.createElement("div");
+            var para = document.createElement("div");
+            newCard.style.border = "2px solid #5da3e9";
+            newCard.style.borderRadius="30px";
+            newCard.style.height = "80px";
+            newCard.style.width = "300px";
+            var weatherIcon = document.createElement("img");
+            weatherIcon.src = chooseWeatherIcon(weatherImg);
+            newCard.style.textAlign = "center";
+            para.innerHTML = weather + temp ;
+            body.appendChild(newCard);
+            newCard.appendChild(weatherIcon);
+            newCard.appendChild(para);
             
-            getApiKey(apiKey)
         }
-    };
-    requestApi.open("GET","ya_api_geocoder.txt",true);
-    requestApi.send();
-
-    function createCard(weather,temp){
-        var newCard = document.createElement("div");
-        var weatherIcon = document.createElement("img");
-        weatherIcon.src = chooseWeatherIcon(weather);
-        newCard.innerHTML = weather + temp ;
-        newCard.appendChild(weatherIcon);
-        body.appendChild(newCard);
-    }
-
-    function chooseWeatherIcon(weather){
-        var src = "";
-        var weatherSplit = weather.split(" ");
-        switch (weather[1]){
-            case "wind": src = "weather_icons/wind.gif";
-                break;
-            case "sun": src = "weather_icons/sun.gif";
-                break;
-            case "snow": src = "weather_icons/snow.gif";
-                break;
-            case "rain": src = "weather_icons/rain.gif";
-                break;
-            case "cloud": src = "weather_icons/cloudy.gif";
-                break;
-            default:
-                src = "weather_icons/sun.gif";
-                break;
-        }
-        return src;
-    }
-
-
-
-    /*requestWeather.onreadystatechange = function(){
-        if(this.status==200){
-            apiKeyWeather = requestWeather.responseText;
-            console.log(requestWeather.responseText)
-            getApiKeyWeather(apiKeyWeather);
+    
+        function chooseWeatherIcon(weatherImg){
+            var urlImg = "http://openweathermap.org/img/w/"+weatherImg+".png";
+            return urlImg;
         }
     }
-    requestWeather.open("GET","ya_api_weather.txt",true);
-    requestWeather.send();*/
 })
